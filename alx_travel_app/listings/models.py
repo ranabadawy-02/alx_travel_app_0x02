f0d7a16001a8bae0792de06f5b3179585ab2663e
ranabadawy-02 <ranamohsen1102@gmail.com>
2025-12-12 21:47:46 +0200

    Create models.py

---
+++ alx_travel_app/listings/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+
+class Payment(models.Model):
+    STATUS_CHOICES = [
+        ("Pending", "Pending"),
+        ("Completed", "Completed"),
+        ("Failed", "Failed"),
+    ]
+
+    booking_reference = models.CharField(max_length=100, unique=True)
+    transaction_id = models.CharField(max_length=100, blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    email = models.EmailField()  # email of the user for sending confirmations
+
+    def __str__(self):
+        return f"{self.booking_reference} - {self.status}"
